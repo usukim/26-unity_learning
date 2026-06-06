80fd332d4de6154b95287e06a505720ca8177b90
usukim <a01046807116@gmail.com>
2026-06-06 22:10:12 +0900

    assignment

--- ASSIGNMENT_01/Assets/Scripts/FloorController.cs
+++ ASSIGNMENT_01/Assets/Scripts/FloorController.cs
@@ -15,12 +15,12 @@ public class FloorController : MonoBehaviour
 
     private void LateUpdate()
     {
-        // 현재 최고 높이에 따라 바닥 넓이를 조정
+        // 현재 최고 높이에 따라 바닥 넓이를 조정 
         Vector2 targetColliderSize = col.size;
         Vector2 targetSpriteSize = sr.size;
         targetColliderSize.x = CameraController.HighestY / 1.8f + 8f;
         targetSpriteSize.x = CameraController.HighestY / 1.8f + 8f;
-        col.size = Vector2.Lerp(col.size, targetColliderSize, Time.deltaTime);
-        sr.size = Vector2.Lerp(sr.size, targetSpriteSize, Time.deltaTime);
+        col.size = Vector2.Lerp(col.size, targetColliderSize, Time.deltaTime * 3);
+        sr.size = Vector2.Lerp(sr.size, targetSpriteSize, Time.deltaTime * 3);
     }
 }
